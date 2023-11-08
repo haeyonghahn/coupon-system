8b4df411204c428369d8c3fb3b1205c7a9904386
haeyonghahn <yong80211@gmail.com>
2023-11-08 22:23:52 +0900

    Feat: 발급가능한 쿠폰개수를 1인당 1개로 제한하기

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -2,8 +2,8 @@ package com.example.api.service;
 
 import org.springframework.stereotype.Service;
 
-import com.example.api.domain.Coupon;
 import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 
@@ -13,15 +13,24 @@ public class ApplyService {
 	private final CouponRepository couponRepository;
 	private final CouponCountRepository couponCountRepository;
 	private final CouponCreateProducer couponCreateProducer;
+	private final AppliedUserRepository appliedUserRepository;
 
 	public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository,
-		CouponCreateProducer couponCreateProducer) {
+		CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository) {
 		this.couponRepository = couponRepository;
 		this.couponCountRepository = couponCountRepository;
 		this.couponCreateProducer = couponCreateProducer;
+		this.appliedUserRepository = appliedUserRepository;
 	}
 
 	public void apply(Long userId) {
+		Long apply = appliedUserRepository.add(userId);
+
+		// 추가된 개수가 1이 아니라면 이 유저는 이미 발급 요청을 했던 유저
+		if (apply != 1) {
+			return;
+		}
+
 		// long count = couponRepository.count();
 		Long count = couponCountRepository.increment();
 
